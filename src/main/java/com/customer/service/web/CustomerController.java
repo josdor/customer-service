f43bbf6f3e6f8b540b8c8afa68f31556d4e254c9
josdor <josdor.r@gmail.com>
2023-02-11 13:05:05 +0100

    feat(controller): ajout logs

--- src/main/java/com/customer/service/web/CustomerController.java
+++ src/main/java/com/customer/service/web/CustomerController.java
@@ -3,6 +3,9 @@ package com.customer.service.web;
 import com.customer.service.dto.CustomerRequestDto;
 import com.customer.service.dto.CustomerResponseDto;
 import com.customer.service.service.CustomerService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +15,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
-
+	
+	private static Logger LOG = LogManager.getLogger(CustomerController.class);
+	
     private CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
@@ -21,6 +26,7 @@ public class CustomerController {
 
     @GetMapping("/customers/{id}")
     public ResponseEntity<CustomerResponseDto> getCustustomer(@PathVariable (value = "id") String id) {
+    	LOG.info("obtenir un client by id : {}", id);
         return new ResponseEntity<>(customerService.getCustomer(id), HttpStatus.OK);
     }
 
